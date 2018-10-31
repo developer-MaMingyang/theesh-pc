c04539d080417e387ba845fe9a10f3712bc07eac
ma <poet0@vip.qq.com>
2018-10-31 15:41:16 +0800

    eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
   parserOptions: {
@@ -10,13 +8,7 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'airbnb'
-  ],
+  extends: ['plugin:vue/essential', 'airbnb-base'],
   // required to lint *.vue files
   plugins: [
     'vue'
