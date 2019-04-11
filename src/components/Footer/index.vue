35f445095d3308625ad1019468d841993bedf87d
ma <poet0@vip.qq.com>
2019-04-11 16:23:24 +0800

    add联系方式

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -5,11 +5,12 @@
 */
 
 <template>
-  <el-footer class="bgcWhi miw1200" height="210px">
+  <el-footer class="bgcWhi miw1200" height="180px">
     <div class="wrap-box clearfix">
-      <div class="mt50 lh43">
+      <div class="mt40 lh30">
         <div>
-          <p class="fz12 tc">联系方式：business@theesh.com</p>
+          <p class="fz12 tc">联系方式：15710020905</p>
+          <p class="fz12 tc">邮箱：business@theesh.com</p>
           <p class="fz12 tc">京ICP备15062124号 Copyright© 2016-2021</p>
         </div>
       </div>
